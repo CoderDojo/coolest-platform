f04706ffce61fc3b1240846ad32b255730b2196f
butlerx <butlerx@notthe.cloud>
2017-12-13 10:54:26 +0000

    fix missing dep

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
-// const favicon = require('serve-favicon');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const protect = require('@risingstack/protect');
+const helmet = require('helmet');
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
@@ -20,73 +20,71 @@ const migrate = require('./database/migrate');
 const routes = require('./routes/index');
 const authControllers = require('./controllers/auth');
 
-const init = () => {
+module.exports = () => {
   const app = express();
   app.locals.bookshelf = bookshelf;
-  return migrate(bookshelf)
-    .then(() => {
-      // uncomment after placing your favicon in /public
-      // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-      app.use(morgan('combined', { stream: logger.stream }));
-      app.use(bodyParser.urlencoded({ extended: false }));
-      app.use(bodyParser.json());
-      app.use(cookieParser());
-      app.use(express.static(path.join(__dirname, 'public')));
+  return migrate(bookshelf).then(() => {
+    app.use(morgan('combined', { stream: logger.stream }));
+    app.use(helmet());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
 
-      // Setup global conf for mailing so we can call this instance from any handler
-      app.locals.mailing = new Mailing(mailingConfig);
+    // Setup global conf for mailing so we can call this instance from any handler
+    app.locals.mailing = new Mailing(mailingConfig);
 
-      // Setup global logger
-      app.locals.logger = logger;
+    // Setup global logger
+    app.locals.logger = logger;
 
-      app.use('/', routes);
-      app.get('/coffee', (req, res) => {
-        res.statusMessage = "I'm a teapot";
-        res
-          .status(418)
-          .json({ message: "I'm a teapot", image: 'https://httpstatusdogs.com/img/418.jpg' });
-      });
+    app.use('/', routes);
+    app.get('/coffee', (req, res) => {
+      res.statusMessage = "I'm a teapot";
+      res
+        .status(418)
+        .json({ message: "I'm a teapot", image: 'https://httpstatusdogs.com/img/418.jpg' });
+    });
 
-      app.use(protect.express.sqlInjection({
-        body: true,
-        loggerFunction: logger.error,
-      }));
+    app.use(protect.express.sqlInjection({
+      body: true,
+      loggerFunction: logger.error,
+    }));
 
-      app.use(protect.express.xss({
-        body: true,
-        loggerFunction: logger.error,
-      }));
+    app.use(protect.express.xss({
+      body: true,
+      loggerFunction: logger.error,
+    }));
 
-      // catch 404 and forward to error handler
-      app.use((req, res, next) => {
-        const err = new Error('Not Found');
-        err.status = 404;
-        res.status(err.status);
-        next(err);
-      });
+    // catch 404 and forward to error handler
+    app.use((req, res, next) => {
+      const err = new Error('Not Found');
+      err.status = 404;
+      res.status(err.status);
+      next(err);
+    });
 
-      passport.use(new JwtStrategy({
-        jwtFromRequest:
-        ExtractJwt.fromExtractors([
+    passport.use(new JwtStrategy(
+      {
+        jwtFromRequest: ExtractJwt.fromExtractors([
           ExtractJwt.fromAuthHeaderAsBearerToken(),
           ExtractJwt.fromUrlQueryParameter('token'),
         ]),
         secretOrKey: authConfig.authSecret,
         maxAge: authConfig.authTimeout,
-      }, authControllers.authenticate));
+      },
+      authControllers.authenticate,
+    ));
 
-      // error handler for any uncaught error
-      app.use((err, req, res, next) => {
-        // set locals, only providing error in development
-        const status = err.status || 500;
-        res.locals.message = err.message;
-        res.locals.error = req.app.get('env') === 'development' ? err : {};
-        // render the error page
-        res.status(status);
-        res.json({ status, msg: err.message });
-      });
-      return app;
+    // error handler for any uncaught error
+    app.use((err, req, res, next) => {
+      // set locals, only providing error in development
+      const status = err.status || 500;
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
+      // render the error page
+      res.status(status);
+      res.json({ status, msg: err.message });
     });
+    return app;
+  });
 };
-
-module.exports = init;
