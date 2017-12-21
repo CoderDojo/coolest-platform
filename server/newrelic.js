98792c4b498d8ea67e5398bba26d1a0b40b07e80
butlerx <butlerx@notthe.cloud>
2017-12-21 09:42:54 +0000

    add new_relic

---
+++ server/newrelic.js
@@ -0,0 +1,19 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  app_name: ['coolest-platform'],
+  agent_enabled: false, // set via NEW_RELIC_ENABLED for production
+  license_key: '', // set via NEW_RELIC_LICENSE_KEY
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info',
+  },
+};
