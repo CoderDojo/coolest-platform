b992b42dcb9ec3a23013d9b5d8c6f5863d8488b7
Daniel Brierton <daniel@brierton.me>
2018-02-05 16:56:45 +0000

    Stop IE users from attempting to use a broken form

--- client/src/Index.vue
+++ client/src/Index.vue
@@ -4,7 +4,13 @@
       <div class="text-center">
         <img class="logo" src="./assets/CoolestProjectsLogo.png" />
       </div>
-      <router-view></router-view>
+      <div v-if="isIE">
+        <h2>Sorry, Internet Explorer is not supported</h2>
+        <div class="row">
+          <p class="col">The browser you are using can not be used to complete this form. Please try again in any modern browser (<a href="https://www.google.com/chrome/">Chrome</a>, <a href="https://www.microsoft.com/en-ie/windows/microsoft-edge">Edge</a>, <a href="https://www.mozilla.org/en-GB/firefox/">Firefox</a>, etc.) to complete this form.</p>
+        </div>
+      </div>
+      <router-view v-else></router-view>
     </div>
   </div>
 </template>
@@ -12,6 +18,11 @@
 <script>
   export default {
     name: 'Index',
+    computed: {
+      isIE() {
+        return window.navigator.userAgent.indexOf('Trident') >= 0;
+      },
+    },
   };
 </script>
 
