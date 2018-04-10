2645e8c2030cfa08312676a69b650f682908a7a8
Wardormeur <wardormeur@gmail.com>
2018-04-10 13:54:57 +0100

    Fix missing library due to my failure w/ git format-patch

--- client/test/unit/specs/event/EventUtilsMixin.spec.js
+++ client/test/unit/specs/event/EventUtilsMixin.spec.js
@@ -19,8 +19,9 @@ describe('Event mixin', () => {
       it('should return a human readable date', () => {
         vm.event = {
           date: '2018-04-04T16:00:00',
+          tz: 'Europe/Dublin',
         };
-        expect(vm.formattedDate).to.equal('Wednesday, April 4, 2018 4:00 PM');
+        expect(vm.formattedDate).to.equal('Wednesday, April 4, 2018 4:00 PM IST');
       });
     });
   });
