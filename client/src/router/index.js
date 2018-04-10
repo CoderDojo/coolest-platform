956f846716a69fe9b1f3808a340ad7e2400221fa
Wardormeur <wardormeur@gmail.com>
2018-04-10 17:38:19 +0100

    USA custom questions (#42)
    * Add questions for USA
    
    * Add seed for tz for usa

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -30,7 +30,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      redirect: '/events/cp-2018',
+      redirect: '/events/na-2018',
       component: Index,
       name: 'Index',
       children: [
@@ -105,7 +105,7 @@ export default new Router({
         {
           path: '',
           name: 'Admin',
-          redirect: 'events/cp-2018',
+          redirect: 'events/na-2018',
           beforeEnter: adminNavGuard,
         },
         {
---
+++ server/database/seeds/06.cp-usa-event.js
@@ -0,0 +1,17 @@
+const uuid = require('uuid/v4');
+
+exports.seed = (knex, Promise) => {
+  const eventDate = new Date();
+  eventDate.setHours(23);
+
+  return knex('event').insert({
+    id: uuid(),
+    date: eventDate,
+    tz: 'America/Los_Angeles',
+    location: 'SOMEWHERE I BELOOONG',
+    name: 'CP North America 2018',
+    slug: 'na-2018',
+    categories: { scratch: 'Scratch', web: 'Websites & Web Games', evolution: 'Evolution' },
+    questions: '["require_power", "require_wifi", "is_hazardous", "other_requirements"]',
+  });
+};
