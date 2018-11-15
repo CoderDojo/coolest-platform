3e6b6e4cdab5759e6923fec9d35a6c2a0026a160
Wardormeur <wardormeur@gmail.com>
2018-11-15 10:26:22 +0000

    Replace usage of createIfNotExists by hasTable + creation This is due to knex deprecating createIfNotExists

--- server/database/migrations/13.add-seating.js
+++ server/database/migrations/13.add-seating.js
@@ -1,11 +1,13 @@
 exports.up = async (knex, Promise) => {
-  await knex.schema.createTableIfNotExists('projects_seating', (table) => {
-    table.uuid('project_id')
-      .index()
-      .references('id')
-      .inTable('project');
-    table.string('seat');
-  });
+  if (!await knex.schema.hasTable('projects_seating')) {
+    await knex.schema.createTable('projects_seating', (table) => {
+      table.uuid('project_id')
+        .index()
+        .references('id')
+        .inTable('project');
+      table.string('seat');
+    });
+  }
   return knex.schema.table('event', table =>
     table.boolean('seating_prepared').defaultTo(false));
 };
