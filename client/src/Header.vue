80bef9ef3b0d50c799c6f0a2fc6e22e1ca96735c
Damien Hogan <damienh@users.noreply.github.com>
2018-11-19 17:06:47 +0000

    Replace the 3 logos with the new coolest project one (#117)
    * Replace the 3 logos with the new coolest project one
    
    * make logo 25% smaller on mobile
    
    * add apple sizes for icons

--- client/src/Header.vue
+++ client/src/Header.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="row row-v-center text-center">
-    <div class="col-2fr">
-      <img class="logo-small logo-cc" src="./assets/CodeClubLogo.svg" />
-    </div>
-    <div class="col-2fr">
-      <img class="logo" src="./assets/CoolestProjectsLogo.png" />
-    </div>
-    <div class="col-2fr">
-      <img class="logo-small" src="./assets/RaspberryPiLogo.png" />
+    <div class="col-6fr">
+      <img class="logo" src="./assets/CoolestProjectsLogo.svg" />
     </div>
   </div>
 </template>
@@ -20,19 +14,12 @@
 
 <style lang="scss">
   .logo {
-    max-width: 180px;
-    margin: 20px;
-    &-small {
-      max-width: 80px;
-    }
-    &-cc {
-      height: 80px;
-    }
-    @media screen and (max-width: 768px) {
-      max-width: 120px;
-      &-small {
-        max-width: 60px;
+    max-width: 300px;
+  }
+
+  @media screen and (max-width: 768px) {
+      .logo {
+        max-width: 225px;
       }
     }
-  }
 </style>
