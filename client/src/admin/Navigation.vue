9bfcaebfe3e01762765a49a70bf5f9da57889f61
Damien Hogan <damienhogan87@gmail.com>
2018-11-27 13:42:55 +0000

    remove projects list from admin nav

--- client/src/admin/Navigation.vue
+++ client/src/admin/Navigation.vue
@@ -3,9 +3,6 @@
     <router-link :to="{ name: 'Admin' }">Coolest Projects Admin</router-link>
     <ul class="nav justify-content-end">
       <slot></slot>
-      <li class="nav-item">
-        <router-link :to="{ name: 'AdminProjects', params: { eventSlug } }" class="nav-link">Projects List</router-link>
-      </li>
     </ul>
   </nav>
 </template>
