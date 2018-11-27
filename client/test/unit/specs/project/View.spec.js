4c6f4301a39727e73f6ef92773a7c377dd10ff74
Daniel Brierton <daniel@brierton.me>
2018-11-27 12:46:27 +0000

    Fixed test

--- client/test/unit/specs/project/View.spec.js
+++ client/test/unit/specs/project/View.spec.js
@@ -1,15 +1,16 @@
 import ProjectView from '!!vue-loader?inject!@/admin/projects/View';
 import vueUnitHelper from 'vue-unit-helper';
 
-describe('ProjectView', () => {
+describe.only('ProjectView', () => {
   let vm;
-  const sandbox = sinon.createSandbox();
-
-  const ProjectViewWithMocks = ProjectView({
-    '@/project/service': { status: { update: sandbox.stub().resolves() } },
-  });
+  let sandbox;
 
   beforeEach(() => {
+    sandbox = sinon.createSandbox();
+
+    const ProjectViewWithMocks = ProjectView({
+      '@/project/service': { status: { update: sandbox.stub().resolves() } },
+    });
     vm = vueUnitHelper(ProjectViewWithMocks);
   });
 
@@ -22,8 +23,9 @@ describe('ProjectView', () => {
     describe('updateProjectStatus', () => {
       it('gets reloaded', async () => {
         vm.project = { status: 'pending' };
-        vm.event = { id: '' };
-        vm.$router = { go: sandbox.stub(), params: {} };
+        vm.event = { id: 'foo' };
+        vm.$route = { params: { projectId: 'bar' } };
+        vm.$router = { go: sandbox.stub() };
         await vm.updateProjectStatus('active');
 
         expect(vm.$router.go).to.have.been.calledOnce;
