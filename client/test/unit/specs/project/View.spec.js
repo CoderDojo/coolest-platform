642e7cbabf4fdf763d884ee9461e7ef7613244dc
Damien Hogan <damienhogan87@gmail.com>
2018-11-27 11:01:10 +0000

    change status buttons in admin view

---
+++ client/test/unit/specs/project/View.spec.js
@@ -0,0 +1,34 @@
+import ProjectView from '!!vue-loader?inject!@/admin/projects/View';
+import vueUnitHelper from 'vue-unit-helper';
+
+describe('ProjectView', () => {
+  let vm;
+  const sandbox = sinon.createSandbox();
+
+  const ProjectViewWithMocks = ProjectView({
+    '@/project/service': { status: { update: sandbox.stub().resolves() } },
+    // '@/project/FetchProjectMixin': sandbox.stub(),
+  });
+
+  beforeEach(() => {
+    vm = vueUnitHelper(ProjectViewWithMocks);
+  });
+
+  afterEach(() => {
+    sandbox.reset();
+    sandbox.restore();
+  });
+
+  describe('methods', () => {
+    describe('updateProjectStatus', () => {
+      it('gets reloaded', async () => {
+        vm.project = { status: 'pending' };
+        vm.event = { id: '' };
+        vm.$router = { go: sandbox.stub(), params: {} };
+        await vm.updateProjectStatus('active');
+
+        expect(vm.$router.go).to.have.been.calledOnce;
+      });
+    });
+  });
+});
