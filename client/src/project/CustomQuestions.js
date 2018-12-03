7a104f64f36c5002225679502514388077cda5c4
Wardormeur <wardormeur@gmail.com>
2018-12-03 09:50:27 +0000

    169-170-172 Link into a new tab (#173)
    add the bursary link to the customQ
    Fix the title of a customQ

--- client/src/project/CustomQuestions.js
+++ client/src/project/CustomQuestions.js
@@ -5,7 +5,7 @@ export default [
   },
   {
     key: 'educational_project',
-    sentence: 'Do you think your project topic is around Education?',
+    sentence: 'Does your project have a social focus or does it address a societal issue?',
   },
   {
     key: 'innovator_stage',
@@ -13,11 +13,11 @@ export default [
   },
   {
     key: 'travel_stipend',
-    sentence: 'Please indicate if you are interested in applying for a travel stipend. If you choose yes, please complete the application <a href="https://docs.google.com/forms/d/e/1FAIpQLSfsaav95uqMds7lZEd6WYc_M6gjzCGR5p5-Mk0kVJsdidyt0A/viewform">here</a>.',
+    sentence: 'Please indicate if you are interested in applying for a travel stipend. If you choose yes, please complete the application <a target="blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfsaav95uqMds7lZEd6WYc_M6gjzCGR5p5-Mk0kVJsdidyt0A/viewform">here</a>.',
   },
   {
     key: 'travel_bursary',
-    sentence: 'Please indicate if you are interested in applying for a travel bursary. If you choose yes, please complete the application <a href="#">here</a>.',
+    sentence: 'Please indicate if you are interested in applying for a travel bursary. If you choose yes, please complete the application <a target="blank" href="https://docs.google.com/forms/d/e/1FAIpQLSdMqqwD8JlvNheWE0LK7SpD5pacRMA9fXflgYrqR0N-VfxtoA/viewform">here</a>.',
   },
   {
     key: 'attendance_prev_year',
