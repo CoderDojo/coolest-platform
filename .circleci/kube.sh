5046ff6a78bf266d1c6878c274a448a707c969c8
Ant Barnes <ant@arayb.com>
2019-11-08 13:06:47 +0000

    Set new event slugs.
    Update slugs so sites default to correct event.

--- .circleci/kube.sh
+++ .circleci/kube.sh
@@ -21,16 +21,16 @@ docker login -u "$DOCKER_USER" -p "$DOCKER_PASS" -e "$DOCKER_EMAIL"
 
 # Build and push
 # INT
-docker build --rm=false -t coderdojo/coolest-platform:"${GIT_SHA1}-${TIMESTAMP}" --build-arg "EVENT_SLUG=int-2019" --build-arg "GOOGLE_ANALYTICS_PROPERTY_ID=UA-112027238-2" .
+docker build --rm=false -t coderdojo/coolest-platform:"${GIT_SHA1}-${TIMESTAMP}" --build-arg "EVENT_SLUG=int-2020" --build-arg "GOOGLE_ANALYTICS_PROPERTY_ID=UA-112027238-2" .
 docker push coderdojo/coolest-platform:"${GIT_SHA1}-${TIMESTAMP}"
 ./kubectl --namespace=coolest-namespace patch deployment coolest-platform -p '{"spec":{"template":{"spec":{"containers":[{"name":"coolest-platform","image":"coderdojo/coolest-platform:'"${GIT_SHA1}-${TIMESTAMP}"'"}]}}}}'
 # USA
-docker build --rm=false -t coderdojo/coolest-platform:usa -t coderdojo/coolest-platform:usa-"${GIT_SHA1}-${TIMESTAMP}" --build-arg "EVENT_SLUG=usa-2019" --build-arg "GOOGLE_ANALYTICS_PROPERTY_ID=UA-112027238-4" .
+docker build --rm=false -t coderdojo/coolest-platform:usa -t coderdojo/coolest-platform:usa-"${GIT_SHA1}-${TIMESTAMP}" --build-arg "EVENT_SLUG=usa-2020" --build-arg "GOOGLE_ANALYTICS_PROPERTY_ID=UA-112027238-4" .
 docker push coderdojo/coolest-platform:usa
 docker push coderdojo/coolest-platform:usa-"${GIT_SHA1}-${TIMESTAMP}"
 ./kubectl --namespace=coolest-namespace-usa patch deployment coolest-platform -p '{"spec":{"template":{"spec":{"containers":[{"name":"coolest-platform","image":"coderdojo/coolest-platform:'usa-"${GIT_SHA1}-${TIMESTAMP}"'"}]}}}}'
 # UK
-docker build --rm=false -t coderdojo/coolest-platform:uk -t coderdojo/coolest-platform:uk-"${GIT_SHA1}-${TIMESTAMP}" --build-arg "EVENT_SLUG=uk-2019" --build-arg "GOOGLE_ANALYTICS_PROPERTY_ID=UA-112027238-5" .
+docker build --rm=false -t coderdojo/coolest-platform:uk -t coderdojo/coolest-platform:uk-"${GIT_SHA1}-${TIMESTAMP}" --build-arg "EVENT_SLUG=uk-2020" --build-arg "GOOGLE_ANALYTICS_PROPERTY_ID=UA-112027238-5" .
 docker push coderdojo/coolest-platform:uk
 docker push coderdojo/coolest-platform:uk-"${GIT_SHA1}-${TIMESTAMP}"
 ./kubectl --namespace=coolest-namespace-uk patch deployment coolest-platform -p '{"spec":{"template":{"spec":{"containers":[{"name":"coolest-platform","image":"coderdojo/coolest-platform:'uk-"${GIT_SHA1}-${TIMESTAMP}"'"}]}}}}'
